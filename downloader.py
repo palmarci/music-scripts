3cfe090591b3c82f256473789b036eb97558e18e
palmarciXd <palmarci@users.noreply.github.com>
2021-11-12 16:58:20 +0100

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,44 @@
+import os
+import youtube_dl
+
+links = {
+	"debug": "https://www.youtube.com/playlist?list=PLAYLISTID"
+}
+
+mainWorkingDir = os.getcwd()
+
+for currentName in links:
+	os.chdir(mainWorkingDir)
+	currentLink = links[currentName]
+	print(f"{currentName}: [{currentLink}]")
+
+	if os.path.exists(os.path.join(mainWorkingDir, currentName)) == False:
+		print("folder '" + currentName + "' does not exist, creating...")
+		os.mkdir(os.path.join(mainWorkingDir, currentName))
+
+	os.chdir(os.path.join(mainWorkingDir, currentName))
+
+	ydl = youtube_dl.YoutubeDL({'quiet': True})
+	video = ""
+	with ydl:
+		result = ydl.extract_info(currentLink, download=False)
+		if 'entries' in result:
+			video = result['entries']
+			for i, item in enumerate(video):
+
+				# found video
+				video = result['entries'][i]
+				print(f"{i+1}/{len(result['entries'])}: {video['title']} [{video['id']}]")
+				
+				# get output name
+				dlFileName = (video["title"] + " - " + video["uploader"]).replace(' - Topic', '').replace('.', '') + "." + video["ext"]
+				
+				# download audio
+				dlCommand = 'yt-dlp -f bestaudio -o "' + dlFileName + '" "https://www.youtube.com/watch?v=' + video["id"] + '"'
+				os.system(dlCommand)
+				
+				# pass it to the converter/normalizer
+				normalizeCommand = "python '" + mainWorkingDir + "/normalizer.py' '" + dlFileName + "'"
+				os.system(normalizeCommand)
+
+	os.chdir(mainWorkingDir)
