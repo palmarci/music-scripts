f07a82911da45a434b5f8a717194405cff61ac22
Pal Marci <20556689+palmarci@users.noreply.github.com>
2023-05-22 00:37:46 +0200

    Update plotMixBPM.py

--- plotMixBPM.py
+++ plotMixBPM.py
@@ -8,14 +8,26 @@ import pywt
 from scipy import signal
 import sys
 import os
+import argparse
+import shutil
 import subprocess
+import tempfile
+
+def convertToWav(input_file, output_dir):
+	print("converting to wav...")
+	temp_dir = tempfile.mkdtemp(dir=output_dir)
+	output_file = os.path.join(temp_dir, "output.wav")
+	ffmpeg_command = f'ffmpeg -loglevel quiet -n -i "{input_file}" "{output_file}"'
+	subprocess.call(ffmpeg_command, shell=True)
+	return output_file
+
 
 def formatSeconds(seconds):
-    hours = seconds // (60*60)
-    seconds %= (60*60)
-    minutes = seconds // 60
-    seconds %= 60
-    return "%02i:%02i:%02i" % (hours, minutes, seconds)
+	hours = seconds // (60*60)
+	seconds %= (60*60)
+	minutes = seconds // 60
+	seconds %= 60
+	return "%02i:%02i:%02i" % (hours, minutes, seconds)
 
 def read_wav(filename, startSecond, endSecond):
 	try:
@@ -34,9 +46,8 @@ def read_wav(filename, startSecond, endSecond):
 		samps = list(array.array("i", wf.readframes(samplesToRead)))
 
 	except Exception as error:
-		print(f"[E] error while reading samples: {e}")
+		print(f"[E] error while reading samples: {error}")
 
-	# sample array, sample rate, length in seconds
 	return [samps, fs]
 
 def peak_detect(data):
@@ -94,7 +105,6 @@ def calculateBpm(data, fs):
 	bpm = 60.0 / peak_ndx_adjusted * (fs / max_decimation)
 	return [bpm, correl]
 
-
 def getBpm(samps, fs, windowSize):
 	data = []
 	correl = []
@@ -102,22 +112,19 @@ def getBpm(samps, fs, windowSize):
 	n = 0
 	nsamps = len(samps)
 	window_samps = int(windowSize * fs)
-	samps_ndx = 0  
+	samps_ndx = 0
 	max_window_ndx = math.floor(nsamps / window_samps)
 	bpms = numpy.zeros(max_window_ndx)
-	
+
 	for window_ndx in range(0, max_window_ndx):
 		data = samps[samps_ndx : samps_ndx + window_samps]
 
 		if not ((len(data) % window_samps) == 0):
 			raise AssertionError(str(len(data)))
 
-		#bpm, correl_temp = calculateBpm(data, fs)
 		calculatedData = calculateBpm(data, fs)
 		if calculatedData == None:
 			continue
-		#if bpm is None:
-		#	continue
 		bpm = calculatedData[0]
 		correl_temp = calculatedData[1]
 
@@ -128,17 +135,10 @@ def getBpm(samps, fs, windowSize):
 
 	return numpy.median(bpms)
 
-def convertToWav(filename):
-	print("converting to wav...")
-	originalFilename = os.path.abspath(filename)
-	workingFilePath = f'/tmp/{os.path.splitext(os.path.basename(originalFilename))[0]}.wav'
-	ffmpegCommand = f'ffmpeg -loglevel quiet -n -i "{originalFilename}" "{workingFilePath}"'
-	subprocess.call(ffmpegCommand, shell=True)
-	return workingFilePath
 
-# how many samples, sample length in seconds, window size(?), safe bpm range
-def plotBpm(filename, settings):
 
+
+def plotBpm(filename, num_samples, sample_length, window_size, min_bpm, max_bpm):
 	fileLength = int(float(subprocess.check_output("ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 '" + filename + "'", shell=True).decode('utf-8').strip()))
 	sampleRate = read_wav(filename, 0, 1)[1]
 
@@ -146,11 +146,11 @@ def plotBpm(filename, settings):
 	timeData = []
 	print(f"{filename}: {fileLength}s at {sampleRate}hz")
 
-	for i in range(0, fileLength, int(fileLength / settings[0])):
-		endTime = i + settings[1]
+	for i in range(0, fileLength, int(fileLength / num_samples)):
+		endTime = i + sample_length
 		wavData = read_wav(filename, i, endTime)
-		currentBpm = getBpm(wavData[0], sampleRate, settings[2])
-		if settings[3][0] < currentBpm < settings[3][1]: 
+		currentBpm = getBpm(wavData[0], sampleRate, window_size)
+		if min_bpm < currentBpm < max_bpm:
 			bpmData.append(currentBpm)
 			timeData.append(formatSeconds(i))
 			print(f"{i:5}s -> {endTime:5}s: {round(currentBpm, 5):6} bpm")
@@ -165,12 +165,28 @@ def plotBpm(filename, settings):
 	plt.xticks(rotation=45)
 	plt.show()
 
-if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
-	filePath = sys.argv[1]
-	if ".wav" not in sys.argv[1]:
-		filePath = convertToWav(sys.argv[1])
-		#print(filePath)
-	plotBpm(filePath, [60, 16, 3.0, [120, 150]])
 
-else:
-	print("usage: python plotBpm.py [filename]")
+def main():
+	parser = argparse.ArgumentParser(description="Calculate and plot BPM from an audio file.")
+	parser.add_argument("filename", help="input audio file")
+	parser.add_argument("-s", "--samples", type=int, default=60, help="number of samples")
+	parser.add_argument("-l", "--length", type=int, default=16, help="sample length in seconds")
+	parser.add_argument("-w", "--window", type=float, default=3.0, help="window size in seconds")
+	parser.add_argument("--min-bpm", type=int, default=120, help="minimum safe BPM range")
+	parser.add_argument("--max-bpm", type=int, default=150, help="maximum safe BPM range")
+	args = parser.parse_args()
+
+	if not os.path.exists(args.filename):
+		print("File not found.")
+		return
+
+	if not args.filename.lower().endswith((".wav", ".wave")):
+		temp_dir = tempfile.gettempdir()
+		converted_file = convertToWav(args.filename, temp_dir)
+		plotBpm(converted_file, args.samples, args.length, args.window, args.min_bpm, args.max_bpm)
+		shutil.rmtree(temp_dir, ignore_errors=True)
+	else:
+		plotBpm(args.filename, args.samples, args.length, args.window, args.min_bpm, args.max_bpm)
+
+if __name__ == "__main__":
+	main()
