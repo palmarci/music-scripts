394f419a087fbea3c16e0928aeeb4e185adadea8
Pal Marci <20556689+palmarci@users.noreply.github.com>
2023-05-30 18:33:50 +0200

    Update cutoff.py

--- cutoff.py
+++ cutoff.py
@@ -3,36 +3,47 @@ import hashlib
 import os
 import sys
 import subprocess
-from multiprocessing import Pool, Manager, Lock
 import fnmatch
 import numpy as np
 from scipy.fft import fft
 from scipy.io import wavfile
 import tempfile
 import matplotlib.pyplot as plt
+import traceback
 
 processed = 0
+filecount = 0
+debug = False
 temp_dir = tempfile.mkdtemp()
-lock = Lock()
 
 def run_command(command):
+	# Execute a shell command and return the output as a string
 	sub_process = subprocess.check_output(command, shell=True)
 	return str(sub_process.decode('utf-8'))
 
 def get_average_by_hertz_range(data_set, wanted_hz_beginning, wanted_hz_end, samples_per_hz):
+	# Calculate the average of a range of Hz values in a dataset
 	data = data_set[int(wanted_hz_beginning * samples_per_hz):int(wanted_hz_end * samples_per_hz)]
-	return sum(data) / len(data)
+	final_cutoff = 0
+	try:
+		final_cutoff = sum(data) / len(data)
+	except Exception as e:
+		print("\nERROR:")
+		print(data, wanted_hz_beginning, wanted_hz_end, samples_per_hz)
+		print(e)
+		print(f"{traceback.format_exc()}")
+	return final_cutoff
 
 def get_song_data(samp_freq, snd):
+	# Get the time array and channel data from a sound file
 	channel = snd[:, 0]
 	time_array = np.arange(0, float(snd.shape[0]), 1)
-
 	time_array = time_array / samp_freq
 	time_array = time_array * 1000
-
 	return time_array, channel
 
-def get_cutoff(file_name, min_search_hz, max_search_hz, hz_step, hz_range):
+def get_cutoff(file_name, min_search_hz, max_search_hz, hz_step, duration):
+	# Calculate the cutoff frequency of a sound file
 	samp_freq, snd = wavfile.read(file_name)
 
 	if snd.dtype == np.dtype('int16'):
@@ -40,7 +51,10 @@ def get_cutoff(file_name, min_search_hz, max_search_hz, hz_step, hz_range):
 	else:
 		snd = snd / (2. ** 31)
 
-	time_array, channel = get_song_data(samp_freq, snd)
+	num_samples = int(samp_freq * duration)
+	snd_middle = snd[num_samples // 2 : num_samples // 2 + num_samples]
+
+	time_array, channel = get_song_data(samp_freq, snd_middle)
 	num_sample_points = len(channel)
 
 	p = fft(channel)
@@ -66,37 +80,64 @@ def get_cutoff(file_name, min_search_hz, max_search_hz, hz_step, hz_range):
 
 	for i in range(min_search_hz, max_search_hz, hz_step):
 		current_avg = get_average_by_hertz_range(10 * np.log10(p), i - hz_step / 2, i + hz_step / 2, samples_per_hz)
-		plotHzs.append([i])
+		plotHzs.append(i)
 		plotDbs.append(current_avg)
 
-	top = max(plotDbs)
-	bot = min(plotDbs)
-	avgs = []
-	low_end = []
 
-	for i in plotDbs:
-		avgs.append(((i / top) * 100) - 100)
+	fall_points = []
+	inclinations = []
+	average_power = np.mean(plotDbs)
 
-	worst = max(avgs)
+	for i in range(len(plotDbs) - 1):
+		if plotDbs[i] > plotDbs[i + 1]:
+			fall_points.append(plotHzs[i + 1])
+			inclination = plotDbs[i] - plotDbs[i + 1]
+			inclinations.append(inclination)
 
-	i = 0
-	while i < len(avgs):
-		if avgs[i] - hz_range / 2 < worst and worst < avgs[i] + hz_range / 2:
-			low_end.append(i)
-		i = i + 1
+	if debug:
+		for i in range(len(fall_points)):
+			print(f'{fall_points[i]}: {inclinations[i]}')
 
-	cutoff = plotHzs[(low_end[0] - 1)][0]
+	
+	current_cutoff_hz = fall_points[inclinations.index(max(inclinations))]
+	current_cutoff_pwr = plotDbs[plotHzs.index(current_cutoff_hz)]
 
-	return cutoff
+	accepted_cutoff_hz = min(plotHzs, key=lambda x:abs(x-max_search_hz))
+	accepted_cutoff_power = plotDbs[plotHzs.index(accepted_cutoff_hz)]
+
+	final_cutoff = 0.0
+
+	if current_cutoff_hz <= (accepted_cutoff_hz - 500): # due to the fact that i cant code
+		
+		threshold_pwr = max(plotDbs) * 1.30 # magic number lol
+		
+		if debug:
+			print(f'current cutoff: {current_cutoff_hz} hz @ {current_cutoff_pwr}, accepted cuttoff {accepted_cutoff_hz} hz @ {accepted_cutoff_power}')
+			print(f'threshold: {threshold_pwr} <=?  accepted: {accepted_cutoff_power}')
+
+		if  threshold_pwr <= accepted_cutoff_power:
+			final_cutoff = accepted_cutoff_hz
+		else:
+			final_cutoff = current_cutoff_hz
+	else:
+		final_cutoff = current_cutoff_hz
+
+	if debug:
+		plt.plot(plotHzs, plotDbs)
+		plt.xlabel('Frequency (Hz)')
+		plt.ylabel('Power (dB)')
+		plt.axvline(x=final_cutoff, color='r')
+		plt.show()
+
+	return final_cutoff
 
 def get_file_hash(filename):
+	# Calculate the MD5 hash of a file's name
 	return hashlib.md5(filename.encode('utf-8')).hexdigest()
 
 def is_audio_format(ext):
-	extensions = ['3gp', '8svx', 'aa', 'aac', 'aax', 'act', 'aiff', 'alac', 'amr', 'ape', 'au', 'awb', 'cda', 'dss',
-				  'dvf', 'flac', 'gsm', 'iklax', 'ivs', 'm4a', 'm4b', 'm4p', 'mmf', 'mogg', 'mp3', 'mpc', 'msv', 'nmf',
-				  'oga', 'ogg', 'opus', 'ra', 'raw', 'rf64', 'rm', 'sln', 'tta', 'voc', 'vox', 'wav', 'webm', 'wma',
-				  'wv']
+	# Check if a file extension corresponds to an audio format
+	extensions = ['flac', 'alac', 'wav', 'aif', 'mp3', 'aac', 'm4a', 'webm', 'opus', 'ogg']
 	ext = ext.replace(".", "")
 	if ext in extensions:
 		return True
@@ -104,60 +145,51 @@ def is_audio_format(ext):
 		return False
 
 def extract_files(directory, pattern):
+	# Recursively extract files from a directory based on a pattern
 	for root, dirs, files in os.walk(directory):
 		for basename in files:
 			if fnmatch.fnmatch(basename, pattern):
 				file_name = os.path.join(root, basename)
 				yield file_name
 
-def process(file_name, min_search_hz, max_search_hz, hz_step, hz_range, filecount, processed):
+def process(file_name, args):
+	global processed
 	cutoff = None
 	file_format = os.path.splitext(file_name)[1]
 
-	if file_format != ".wav":        
+	#args.max_search_hz += 1000 # hack
+
+	if file_format != ".wav":
 		temp_file_name = os.path.join(temp_dir, get_file_hash(file_name) + '.wav')
 		run_command(f'ffmpeg -y -nostats -loglevel panic -hide_banner -i "{file_name}" "{temp_file_name}"')
-		cutoff = get_cutoff(temp_file_name, min_search_hz, max_search_hz, hz_step, hz_range)
+		cutoff = get_cutoff(temp_file_name, args.min_search_hz, args.max_search_hz, args.hz_step, args.duration)
 		os.remove(temp_file_name)
 	else:
-		cutoff = get_cutoff(file_name, min_search_hz, max_search_hz, hz_step, hz_range)
+		cutoff = get_cutoff(file_name, args.min_search_hz, args.max_search_hz, args.hz_step, args.duration)
 
-	with lock:
-		processed.value += 1
-		current_processed = processed.value
+	processed += 1
 
-	if cutoff >= 20000:
-		print(f"    [{current_processed}/{filecount.value}] - {file_name}: {cutoff}")
+	if cutoff >= 19000:
+		print(f"    [{processed}/{filecount}] - '{file_name}': {cutoff}")
 	else:
-		print(f"!!! [{current_processed}/{filecount.value}] - {file_name}: {cutoff}")
+		print(f"!!! [{processed}/{filecount}] - '{file_name}': {cutoff}")
 
 def main():
-	manager = Manager()
-	filecount = manager.Value('i', 0)
-	processed = manager.Value('i', 0)
+	global filecount, debug
 
-	parser = argparse.ArgumentParser(description="""Detect the cutoff frequency of audio files in a given directory.
-		\r\n
-		WARNING: high CPU and memory usage. Subprocesses can get killed by the kernel if the system is out of memory. 
-		It is highly recommended to have swap space, otherwise the script may hang and not exit properly, waiting for the dead subprocesses. 
-		If the issue persists, you may need to lower the number of threads or investigate if there is a memory leak.""")
+	parser = argparse.ArgumentParser(description="Detect the cutoff frequency of audio files in a given directory.")
 
 	parser.add_argument('folder', help='The folder to search for audio files')
 	parser.add_argument('--min-search-hz', type=int, default=10000, help='The minimum search frequency in Hz')
-	parser.add_argument('--max-search-hz', type=int, default=24000, help='The maximum search frequency in Hz')
-	parser.add_argument('--hz-step', type=int, default=500, help='The step size for frequency search in Hz')
-	parser.add_argument('--hz-range', type=int, default=12, help='The range of frequencies to consider in Hz')
+	parser.add_argument('--max-search-hz', type=int, default=20000, help='The maximum search frequency in Hz')
+	parser.add_argument('--hz-step', type=int, default=100, help='The step size for frequency search in Hz')
+	parser.add_argument('--debug', type=bool, default=False, help='Plot debug graph')
+	parser.add_argument('--duration', type=int, default=60, help='Duration of the portion of the song to analyze in seconds')
+
 
 	args = parser.parse_args()
 
-	cpu = os.cpu_count()
-	if cpu >= 8:
-		cpu = cpu -2
-	else:
-		cpu = cpu -1 
-
-	print(f'Starting with {cpu} threads')
-	pool = Pool(processes=cpu)
+	debug = args.debug
 
 	file_list = []
 
@@ -169,11 +201,12 @@ def main():
 			print(f"Skipping file {file_name}, as it is not an audio file.")
 			continue
 
-	filecount.value = len(file_list)
-	print(f'Found {filecount.value} audio files in the directory')
+	file_list.sort()
+	filecount = len(file_list)
+	print(f'Found {filecount} audio files in the directory')
 
-	pool.starmap(process, [(file_name, args.min_search_hz, args.max_search_hz, args.hz_step, args.hz_range, filecount, processed)
-						   for file_name in file_list])
+	for f in file_list:
+		process(f, args)
 
 if __name__ == '__main__':
 	main()
