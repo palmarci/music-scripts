1a3f6738c0b01799faf77f44f5a3b9368ea7b7e1
Pal Marci <20556689+palmarci@users.noreply.github.com>
2023-10-16 14:41:28 +0200

    Fixed output file logic

--- rb_mix_converter.py
+++ rb_mix_converter.py
@@ -4,20 +4,19 @@ import datetime
 import argparse
 import shutil
 
-output_folder = '/tmp/converted/'
-
 def convert_to_mp3(input_file, output_file):
 	# Convert WAV to MP3 using ffmpeg
 	subprocess.call(['ffmpeg', '-i', input_file, '-b:a', '320k', output_file])
 
-def convert_folder_to_mp3(folder_path, output_format):
+def convert_folder_to_mp3(input_folder, output_folder, output_format):
 	
 	if os.path.isdir(output_folder):
-		shutil.rmtree(output_folder)
+		raise Exception("Output folder already exists!")
+		#shutil.rmtree(output_folder)
 	
 	os.mkdir(output_folder)
 	
-	for root, dirs, files in os.walk(folder_path):
+	for root, dirs, files in os.walk(input_folder):
 		for file in files:
 			if file.endswith('.wav'):
 				wav_file = os.path.join(root, file)
@@ -28,8 +27,8 @@ def convert_folder_to_mp3(folder_path, output_format):
 				
 				# Generate output filename
 				output_filename = modified_datetime.strftime(output_format)
-				#output_file = os.path.join(root, output_filename + '.mp3')
-				output_file = f'{output_folder}{output_filename}.mp3'
+				output_file = os.path.join(output_folder, output_filename + '.mp3')
+				#output_file = f'{output_folder}{output_filename}.mp3'
 
 				# Convert WAV to MP3
 				convert_to_mp3(wav_file, output_file)
@@ -38,9 +37,10 @@ def convert_folder_to_mp3(folder_path, output_format):
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Convert WAV files in a folder to MP3.')
-parser.add_argument('folder', type=str, help='Folder path containing WAV files')
-parser.add_argument('--output-format', type=str, default="%Y_%m_%d__%H_%M", help='Output filename format (default: %(default)s)')
+parser.add_argument('input_folder', type=str, help='Input folder path containing the WAV files')
+parser.add_argument('output_folder', type=str, help='Output folder path containing the MP3 files')
+parser.add_argument('--output-text-format', type=str, default="%Y_%m_%d__%H_%M", help='Output filename date format (default: %(default)s)')
 args = parser.parse_args()
 
 # Convert folder to MP3
-convert_folder_to_mp3(args.folder, args.output_format)
+convert_folder_to_mp3(args.input_folder, args.output_folder, args.output_text_format)
